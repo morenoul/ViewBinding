43935bf81cf7b9d53e5259069cb9d796f38dacc1
marc <morenoul@gmail.com>
2023-01-20 23:07:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.viewbinding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.button.setOnClickListener {
+            Toast.makeText(this, "Estas usando ViewBinding!", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
